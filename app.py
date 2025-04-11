3b88363f553652e809e2390698bf764b8f92f7ac
Vlad Sunny <68503186+VladSunny@users.noreply.github.com>
2025-04-12 00:01:34 +0300

    upd

--- app.py
+++ app.py
@@ -1,26 +1,70 @@
 from flask import Flask, jsonify, request
+import os
+from supabase import create_client, Client
+from io import StringIO
+import pandas as pd
+
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(supabase_url, supabase_key)
 
 app = Flask(__name__)
 
-# Пример данных (можно заменить на базу данных)
-data_store = {
-    "message": "Hello from Flask backend!",
-    "items": ["Item 1", "Item 2", "Item 3"]
-}
-
-# GET-запрос для получения данных
-@app.route('/api/data', methods=['GET'])
 def get_data():
-    return jsonify(data_store)
+    response = (
+        supabase.table("Payments")
+        .select("*")
+        .csv()
+        .execute()
+    )
 
-# POST-запрос для добавления нового элемента
-@app.route('/api/data', methods=['POST'])
-def add_item():
-    new_item = request.json.get('item')
-    if new_item:
-        data_store['items'].append(new_item)
-        return jsonify({"status": "success", "message": f"Added {new_item}"}), 201
-    return jsonify({"status": "error", "message": "No item provided"}), 400
+    df = pd.read_csv(StringIO(response.data), index_col=0)
+    df.drop(columns=['created_at'], inplace=True)
+
+    return df
+
+@app.route('/api/data/price-quantity/<uuid>', methods=['GET'])
+def get_data_price_quantity(uuid):
+    df = get_data()
+    df = df[df['uuid'] == uuid][['product_name', 'quantity', 'price', 'purchase_date']]
+
+    tmp = df.groupby(['product_name', 'purchase_date']).sum().reset_index()
+
+    # Уникальные даты
+    labels = tmp['purchase_date'].unique().tolist()
+
+    # Подготовка данных для Quantity
+    quantity_datasets = []
+    for product_name, group in tmp.groupby('product_name'):
+        quantity_datasets.append({
+            "label": product_name,
+            "data": group.set_index('purchase_date')['quantity'].reindex(labels, fill_value=0).tolist()
+        })
+
+    # Подготовка данных для Price
+    price_datasets = []
+    for product_name, group in tmp.groupby('product_name'):
+        price_datasets.append({
+            "label": product_name,
+            "data": group.set_index('purchase_date')['price'].reindex(labels, fill_value=0).tolist()
+        })
+
+    # Формирование JSON
+    chart_data = {
+        "quantity": {
+            "labels": labels,
+            "datasets": quantity_datasets
+        },
+        "price": {
+            "labels": labels,
+            "datasets": price_datasets
+        }
+    }
+
+    return jsonify(chart_data)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)

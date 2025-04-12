277a699fe7c016a1451f74b0d037acc5452793ef
VladG00dman <68503186+VladSunny@users.noreply.github.com>
2025-04-12 12:39:32 +0300

    upd colors

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ caramel_latte_palette = [
     {"backgroundColor": "rgba(139, 69, 19, 0.6)", "borderColor": "rgba(139, 69, 19, 1)"},  # Кофе с молоком
     {"backgroundColor": "rgba(222, 184, 135, 0.6)", "borderColor": "rgba(222, 184, 135, 1)"},  # Светло-коричневый
     {"backgroundColor": "rgba(245, 222, 179, 0.6)", "borderColor": "rgba(245, 222, 179, 1)"},  # Бежевый-темный
-    {"backgroundColor": "rgba(255, 239, 213, 0.6)", "borderColor": "rgba(255, 239, 213, 1)"},  # Кремовый-насыщенный
+    # {"backgroundColor": "rgba(255, 239, 213, 0.6)", "borderColor": "rgba(255, 239, 213, 1)"},  # Кремовый-насыщенный
 ]
